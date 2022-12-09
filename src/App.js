3c73b43600a715faf73392c007b0be509fd17c8b
Henrique Coutinho <henrique.coutinho@engenharia.ufjf.br>
2022-12-09 00:27:49 -0300

    Calculadora feita em React V1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import {Container, Content, Row, Column} from "./styles"
+import Button from "./components/Button";
+import Input from "./components/Input";
+import { useState } from "react";
 
 function App() {
+
+  const [currentNumber, setCurrentNumber] = useState("0");
+
+  const addNumber = (number) => {
+
+    if(number === "C") {
+
+      setCurrentNumber("0");
+      return;
+
+    }else if(number === "="){
+
+      let op = /(\+)|(-)|(x)|(\/)/.exec(currentNumber);
+
+      if(!op) return;
+
+      let currentNumberArr = currentNumber.split(op[0]);
+      setCurrentNumber(operate(currentNumberArr[0],currentNumberArr[1],op[0]));
+      return;
+      
+    }else if(/(\+)|(-)|(x)|(\/)/.test(number)){
+      if(/(\+)|(-)|(x)|(\/)/.test(currentNumber)) return;
+    }
+    setCurrentNumber(prev => (prev === "0" ? "" : prev) + number);
+  }
+
+  const operate = (firstNumber, secondNumber, op) =>{
+
+    let result;
+    switch (op){
+      case "+":
+        result = parseFloat(firstNumber) + parseFloat(secondNumber);
+      break;
+      case "-":
+        result = parseFloat(firstNumber) - parseFloat(secondNumber);
+      break;
+      case "x":
+        result = parseFloat(firstNumber) * parseFloat(secondNumber);
+      break;
+      case "/":
+        result = parseFloat(firstNumber) / parseFloat(secondNumber);
+      break;
+    }
+    return result;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+        <Row>
+          {["x","/",".","C","7","8","9","-","4","5","6","+","1","2","3","="]
+            .map(e => <Button label={e} onClick={() => addNumber(e)}/>)}
+        </Row>
+      </Content>
+    </Container>
   );
 }
 
---
+++ src/components/Input/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: red;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: red;
+        border: 0;
+        padding: 0 10px;
+
+        font-size: 24px;
+    }
+`;
